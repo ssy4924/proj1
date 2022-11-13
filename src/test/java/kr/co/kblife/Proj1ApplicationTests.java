b583ffce8eccb11fef06b094bbde111819bd9920
seong <siyoung.seong@kblife.co.kr>
2022-11-14 07:57:54 +0900

    프로젝트 템플릿 배포

---
+++ src/test/java/kr/co/kblife/Proj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kblife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
